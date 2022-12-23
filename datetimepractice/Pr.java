fb6e422b3eb2f929b446453e12a22e41fd75b596
Asthethi <amritpalsinghthethi@gmail.com>
2022-12-23 16:43:28 +0530

    Commit

--- datetimepractice/Pr.java
+++ datetimepractice/Pr.java
@@ -1,25 +1,15 @@
-package coreJavaPractice.stack;
+
+package datetime;
 
 import java.text.ParseException;
-import java.time.LocalDate;
 import java.time.Year;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 
-public class Pr {
+public class DaysBetween {
+	private static HashMap<Integer, Integer> monthsMap = new HashMap<>();
 
 	public static void main(String[] args) throws ParseException {
-		// TODO Auto-generated method stub
-		System.out.println(findDays(15, 12, 1993, 15, 12, 1994));
 
-	}
-
-	static int findDays(int day_date1, int month_date1, int year_date1, int day_date2, int month_date2, int year_date2)
-			throws ParseException {
-		/// logic to calculate number of days between given two dates
-
-		HashMap<Integer, Integer> monthsMap = new HashMap<>();
 		monthsMap.put(1, 31);
 		monthsMap.put(2, 28);
 		monthsMap.put(3, 31);
@@ -32,54 +22,49 @@ public class Pr {
 		monthsMap.put(10, 31);
 		monthsMap.put(11, 30);
 		monthsMap.put(12, 31);
+		System.out.println(findDays(01, 01, 2001, 31, 12, 2005));
+	}
 
-		int days = 0;
-		if (Year.of(year_date1).isLeap())
-			days = days + 1;
+	private static int findDays(int day_date1, int month_date1, int year_date1, int day_date2, int month_date2,
+			int year_date2) throws ParseException {
+		/// logic to calculate number of days between given two dates
 
-		for (int i = month_date1; i <= 12; i++) {
-			int numOfDaysInMonth = monthsMap.get(month_date1);
-			if (i == month_date1)
-				days = days + (numOfDaysInMonth - day_date1);
-			else
-				days = days + numOfDaysInMonth;
+		int startDateRemainingDays = 0;
 
-		}
+		startDateRemainingDays = Year.of(year_date1).isLeap() ? startDateRemainingDays + 1 : startDateRemainingDays;
 
-		for (int yr = year_date1 + 1; yr <= year_date2; yr++) {
+		startDateRemainingDays = calculateDaysInYear(month_date1, day_date1);
 
+		int counter = 0;
+
+		for (int yr = year_date1 + 1; yr < year_date2; yr++) {
 			if (Year.of(yr).isLeap())
-				days = days + 366;
-			else
-				days = days + 365;
+				counter++;
 		}
-		
-		int newDays = 0;
-		for(int j=1; j<=month_date2;j++) {
-			if (j == month_date2)
-				newDays = newDays + (monthsMap.get(j) - day_date2);
+
+		int daysInBetween = 365 * ((year_date2) - (year_date1 + 1)) + counter;
+
+		int yearsDay = Year.of(year_date2).isLeap() ? 366 : 365;
+		yearsDay = yearsDay - calculateDaysInYear(month_date2, day_date2);
+
+		return startDateRemainingDays + yearsDay + daysInBetween;
+
+	}
+
+	// It Calculates the number of days remaining from the provided month and date
+	// till the last month of the year
+	private static int calculateDaysInYear(int month, int date) {
+		int daysBetween = 0;
+		for (int i = month; i <= 12; i++) {
+			int numOfDaysInMonth = monthsMap.get(i);
+			if (i == month)
+				daysBetween = daysBetween + (numOfDaysInMonth - date);
 			else
-				newDays = newDays + monthsMap.get(j);
+				daysBetween = daysBetween + numOfDaysInMonth;
+
 		}
-		
 
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd MM yyyy");
-		String inputString1 = "15 12 1993";
-		String inputString2 = "15 12 1994";
-		LocalDate date1 = LocalDate.parse(inputString1, dtf);
-		LocalDate date2 = LocalDate.parse(inputString2, dtf);
-		long daysBetween = ChronoUnit.DAYS.between(date1, date2);
-		System.out.println(daysBetween);
-//		
-//		SimpleDateFormat myFormat = new SimpleDateFormat("dd MM yyyy");
-//
-//		Date date11 = myFormat.parse(inputString1);
-//	    Date date22 = myFormat.parse(inputString2);
-//	    long diff = date22.getTime() - date11.getTime();
-//	    System.out.println ("Days: " + TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS));
-
-		return days;
-
+		return daysBetween;
 	}
 
 }
