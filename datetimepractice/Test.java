0cfbdc5ee7b9b18860a022f1c2b4632af1cdcbbe
AMRITPAL SINGH THETHI <amritpalsinghthethi@gmail.com>
2024-05-22 12:40:24 +0530

    formatted

--- datetimepractice/Test.java
+++ datetimepractice/Test.java
@@ -1,9 +1,11 @@
-public class Test{
+public class Test {
 
     int Amrit = 1;
-    public TestO(){}
 
-    public static void main(){
+    public TestO() {
+    }
+
+    public static void main() {
         System.out.println("Test Class");
     }
 }
