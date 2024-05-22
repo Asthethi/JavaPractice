19031f5188bd3f394a5455856c822ab1f4583319
AMRITPAL SINGH THETHI <amritpalsinghthethi@gmail.com>
2024-05-22 12:32:44 +0530

    test cmd

--- review_script.py
+++ review_script.py
@@ -30,7 +30,7 @@ def main():
             #subprocess.run(["pylint", java_file])
             #print(java_file)
 
-    cmd = ['java','-version']
+    cmd = ['java','-jar', checkstyle_jar, '-c', config_file] + java_files
     result = subprocess.run(cmd,capture_output=True, text=True)
 
     if result.returncode == 0:
