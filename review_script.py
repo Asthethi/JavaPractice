0b11e70ab374f82f7e3925d23b27c230b9dbe116
AMRITPAL SINGH THETHI <amritpalsinghthethi@gmail.com>
2024-05-22 12:30:25 +0530

    test cmd

--- review_script.py
+++ review_script.py
@@ -35,6 +35,7 @@ def main():
 
     if result.returncode == 0:
         print("Checkstyle passed with no violations.")
+        print(result)
     else:
         print("Checkstyle violations found:")
         print(result.stdout)
