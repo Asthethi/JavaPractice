8ed865b1350214414c5b0759694da4910d8ec806
AMRITPAL SINGH THETHI <amritpalsinghthethi@gmail.com>
2024-05-27 15:10:10 +0530

    Update Test.java

--- datetimepractice/Test.java
+++ datetimepractice/Test.java
@@ -1,6 +1,10 @@
 public class Test {
 
     int Amrit = 1;
+    String StringA = "Test String";
+    String StringB = "Test String";
+    String StringC = "Test String";
+    String StringD = "Test String";
 
     public Test() {
     }
